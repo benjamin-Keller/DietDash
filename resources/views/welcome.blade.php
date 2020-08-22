4290924463529338706e531e1979cd6278166e5f
Benjamin Keller <zhero240@gmail.com>
2020-08-22 22:03:02 +0200

    Added Favicons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,11 @@
 
         <title>DietDash</title>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('/site.webmanifest') }}">
+
         <!-- Bootstrap core CSS -->
         <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
