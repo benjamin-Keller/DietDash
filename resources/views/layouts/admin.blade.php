8f9c2b0a551edb4d491c13219672a60f6d0ae196
Benjamin Keller <zhero240@gmail.com>
2020-08-26 16:58:54 +0200

    Fixed assets not loading

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,9 +19,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <title>{{ config('app.name') }}</title>
 
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="bower_components/admin-lte/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('bower_components/admin-lte/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="bower_components/admin-lte/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{ asset('bower_components/admin-lte/dist/css/adminlte.min.css') }}">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <script
@@ -179,17 +179,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <div class="float-right d-none d-sm-inline">
         </div>
         <!-- Default to the left -->
-        <strong>Copyright &copy; {{date('yy')}} <a href="http://netiquette.co.za">Netiquette</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; {{ date('yy') }} <a href="http://netiquette.co.za">Netiquette</a>.</strong> All rights reserved.
     </footer>
 </div>
 <!-- ./wrapper -->
 
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
-<script src="bower_components/admin-lte/plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('bower_components/admin-lte/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="bower_components/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('bower_components/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="bower_components/admin-lte/dist/js/adminlte.min.js"></script>
+<script src="{{ asset('bower_components/admin-lte/dist/js/adminlte.min.js') }}"></script>
 </body>
 </html>
