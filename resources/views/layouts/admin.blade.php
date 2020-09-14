7e22a2bccf6d15c6d494695c254e972eb282f135
Benjamin Keller <zhero240@gmail.com>
2020-09-14 23:11:23 +0200

    Fixed bad route

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -101,7 +101,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #400040">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="{{ route('home') }}" class="brand-link">
             <img src="{{ asset('img/logo.png') }}" alt="DietDash Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">DietDash</span>
