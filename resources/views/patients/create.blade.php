b74040cdb034cf39f2953ecff282a6b06a26cad2
Benjamin Keller <zhero240@gmail.com>
2020-09-15 14:13:46 +0200

    Added demographics Front-end (NOT backend)

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -48,6 +48,57 @@
                        {{ Form::label('Email', 'Email') }}
                         {{ Form::text('Email', '', ['class'=> 'form-control']) }}
                     </div>
+
+                    <div class="accordion" id="wh">
+                        <div class="card">
+                            <div class="card-header" id="headingWH" type="button" data-toggle="collapse" data-target="#collapseWH" aria-expanded="true" aria-controls="collapseWH">
+                                <h2 class="btn text-purple text-bold" >
+                                    Demographics
+                                </h2>
+                            </div>
+                            <div id="collapseWH" class="collapse show" aria-labelledby="headingWH" data-parent="#wh">
+                                <div class="pt-3 pr-5 pb-3 pl-5">
+                                    <h3>General</h3>
+
+                                    <div class="form-group">
+                                        {{ Form::label('home_language', 'Home Language') }}
+                                        {{ Form::text('home_language', '', ['class'=> 'form-control']) }}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('household_size', 'Household size') }}
+                                        {{ Form::text('household_size', '', ['class'=> 'form-control']) }}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('approx_Income', 'Approximate Income') }}
+                                        {{ Form::text('approx_Income', '', ['class'=> 'form-control']) }}
+                                    </div>
+                                    <hr />
+                                    <h3>Address</h3>
+                                    <div class="form-group">
+                                        {{ Form::label('address_ln1', 'Address Line 1') }}
+                                        {{ Form::text('address_ln1', '', ['class'=> 'form-control']) }}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('address_ln2', 'Address Line 2') }}
+                                        {{ Form::text('address_ln2', '', ['class'=> 'form-control']) }}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('city', 'City') }}
+                                        {{ Form::text('city', '', ['class'=> 'form-control']) }}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('province', 'Province') }}
+                                        {{ Form::text('province', '', ['class'=> 'form-control']) }}
+                                    </div>
+                                    <div class="form-group">
+                                        {{ Form::label('zip', 'Zip Code') }}
+                                        {{ Form::text('zip', '', ['class'=> 'form-control']) }}
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <hr />
                     <br />
 
