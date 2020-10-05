034c81b68ed541e8e6abd8d72bfbaf4bf5971854
Benjamin Keller <zhero240@gmail.com>
2020-10-05 03:33:22 +0200

    Added Disease Stats and Graph

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Event;
 use App\Patient;
+use App\Patient_Disease;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -19,13 +20,43 @@ class StatsController extends Controller
 
         $upcomings = Event::WhereDate('start', '>=', $now)->where('user_id', '=', Auth::id())->where('Deleted', '=', '0')->get();
 
+        $arthritis = Patient_Disease::where('arthritis', '=', '1')->count();
+        $renal_failure = Patient_Disease::where('renal_failure', '=', '1')->count();
+        $dehydration = Patient_Disease::where('dehydration', '=', '1')->count();
+        $underweight = Patient_Disease::where('underweight', '=', '1')->count();
+        $diabetes = Patient_Disease::where('diabetes', '=', '1')->count();
+        $hiv = Patient_Disease::where('hiv', '=', '1')->count();
+        $mam = Patient_Disease::where('mam', '=', '1')->count();
+        $wasted = Patient_Disease::where('wasted', '=', '1')->count();
+        $epilepsy = Patient_Disease::where('epilepsy', '=', '1')->count();
+        $pneumonia = Patient_Disease::where('pneumonia', '=', '1')->count();
+        $sam = Patient_Disease::where('sam', '=', '1')->count();
+        $hypertension = Patient_Disease::where('hypertension', '=', '1')->count();
+        $tb = Patient_Disease::where('tb', '=', '1')->count();
+        $stunted = Patient_Disease::where('stunted', '=', '1')->count();
+
         return view('stats.index', [
             'userCount' => User::count(),
             'today' => $todays->count(),
             'upcoming' => $upcomings->count(),
             'bookings' => Event::all()->where('user_id', '=', Auth::id())->toArray(),
             'patient_event' => DB::table('patients')->select('id', DB::raw("CONCAT(FirstName, ' ', LastName) AS full_name"))->where('user_id', 'like', Auth::id())->where('Deleted', 'like', '0')->get()->sortBy('full_name')->pluck('full_name', 'id'),
-            'patients' => Patient::all()->where('user_id', Auth::user()->id)->where('Deleted', 'like', '0')->all()]);
+            'patients' => Patient::all()->where('user_id', Auth::user()->id)->where('Deleted', 'like', '0')->all(),
+            'arthritis' => $arthritis,
+            'renal_failure' => $renal_failure,
+            'dehydration' => $dehydration,
+            'underweight' => $underweight,
+            'diabetes' => $diabetes,
+            'hiv' => $hiv,
+            'mam' => $mam,
+            'wasted' => $wasted,
+            'epilepsy' => $epilepsy,
+            'pneumonia' => $pneumonia,
+            'sam' => $sam,
+            'hypertension' => $hypertension,
+            'tb' => $tb,
+            'stunted' => $stunted,
+            ]);
     }
 
 }
--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -105,6 +105,318 @@
                     </div>
                 </div>
             </div>
+            <h4 class="pt-3 mb-0">Diseases</h4>
+            <div class="row">
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.index') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Arthritis</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $arthritis }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Renal Failure</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $renal_failure }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Dehydration</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $dehydration }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Underweight</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $underweight }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.index') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Diabetes</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $diabetes }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">HIV</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $hiv }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">MAM</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $mam }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Wasted</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $wasted }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.index') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Epilepsy</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $epilepsy }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Pneumonia</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $pneumonia }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">SAM</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $sam }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Hypertension</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $hypertension }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div><div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">TB</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $tb }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col pt-2">
+                    <div class="card shadow h-100 py-2">
+                        <!-- Total Patients Card -->
+                        <a href="{{ route('patients.deleted') }}" style="text-decoration: none; color: black;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-md font-weight-bold mb-1 inverted" style="color: #206cbe">Stunted</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $stunted }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="pb-3">
+                <div id="" class="chart-container-style">
+                    <canvas id="macro-chart" class="chart-container-style"></canvas>
+                </div>
+            </div>
+
+            <script>
+                create_chart([{{ $arthritis }}, {{ $renal_failure }}, {{ $dehydration }},
+                    {{$underweight}}, {{$diabetes}}, {{$hiv}},
+                    {{$mam}}, {{$wasted}}, {{$epilepsy}},
+                    {{$pneumonia}}, {{$sam}}, {{$hypertension}},
+                    {{$tb}}, {{$stunted}}]);
+
+                function create_chart(sendData) {
+                    var ctx = document.getElementById('macro-chart').getContext("2d");
+                    if(window.myChart != undefined)
+                        window.myChart.destroy();
+                    window.myChart = new Chart(ctx, {});
+
+                    window.myChart = new Chart(ctx, {
+                        type: 'doughnut',
+                        data: {
+                            labels: ["Arthritis", "Renal Failure", "Dehydration",
+                                "Underweight", "Diabetes", "HIV",
+                                "MAM", "Wasted", "Epilepsy",
+                                "Pneumonia", "SAM", "Hypertension",
+                                "TB", "Stunted"],
+                            datasets: [{
+                                label: "Population (millions)",
+                                backgroundColor: ["#EABFCB","#2F004F","#2fbc63",
+                                    "#3ea4a4", "#c191a1","#c5dca0","#a4508b","#95BF8F",
+                                    "#f5f2b8", "#5F0A87", "#E2E1B9", "#3E000C", "#7C6A0A", "#0A1128"],
+                                data: sendData
+                            }]
+                        },
+                        options: {
+                            title: {
+                                display: true,
+                                text: 'Diseases'
+                            }
+                        }
+                    });
+                }
+            </script>
+            <style>
+
+                @media (min-width:800px) {
+                    /* big landscape tablets, laptops, and desktops */
+                    .chart-container-style {
+                        height:80vh;
+                        width:70vw;
+                        margin: 0 auto;
+                        text-align: center;
+
+                    }
+                }
+
+                @media (min-width:1281px) {
+                    /* hi-res laptops and desktops */
+                    .chart-container-style {
+                        height:30vh;
+                        width:30vw;
+                        margin: 0 auto;
+                        text-align: center;
+
+                    }
+                }
+            </style>
         </div>
 @endsection
 
